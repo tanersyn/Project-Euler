c9d8942e514f511db7d98509f1e83f63fd0d3821
Taner Sayın <50462952+tanersyn@users.noreply.github.com>
2020-07-13 23:16:10 +0300

    Update and rename Problem3.py to Problem03.py

--- Problem03.py
+++ Problem03.py
@@ -1,27 +1,19 @@
 # 13195'in asal çarpanları 5,7,13 ve 29'dur
 # 600851475143 sayısının en büyük asal çarpanı nedir?
-
 i = 2
 sayı = 600851475143 
 liste=[]
-
-while i < sayı:
-    
+while (i < sayı):
     if sayı % i == 0:
-
         sayac = 0
-
         for j in range(2,i):
             if (i % j == 0):
                 sayac +=1
                 break
-
         if (sayac != 0):
             print('Asal Değil')
         else:
             print('Asal')
-
         print(i)
         liste.append(i)
-
     i += 1
