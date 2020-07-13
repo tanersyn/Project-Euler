836d172f2e0af9108e1611b4bfdc207a79f56537
Taner Sayın <50462952+tanersyn@users.noreply.github.com>
2020-07-13 23:05:22 +0300

    Update and rename Problem2.py to Problem02.py

--- Problem02.py
+++ Problem02.py
@@ -1,17 +1,12 @@
 # Fibonacci Serisinde 4 milyonu aşmayacak şekilde oluşan çift sayıların toplamını bulan program
 def hesapla():
-
     cevap = 0
     x = 1
     y = 2
-
     while (x < 4000000):
         if (x % 2 == 0):
             cevap += x
-
         x,y = y, x+y
-
     return print('Fibonacci Serisi İçerisindeki Çift Sayıların Toplamı :',cevap)
-
 if __name__ == "__main__":
 	print(hesapla())
