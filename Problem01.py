643e341873b8d90ce3b5eedc91eb332cd6bdac6d
Taner Sayın <50462952+tanersyn@users.noreply.github.com>
2020-07-13 23:04:28 +0300

    Update and rename Problem1.py to Problem01.py

--- Problem01.py
+++ Problem01.py
@@ -1,10 +1,7 @@
 # 10' un altında 3 VEYA 5'in katları olan tüm doğal sayıları listelersek [3,5,6,9] ve toplamları 23 olur.
 # 1000'in altındaki 3 VEYA 5'in tüm katlarının toplamlarını bul
-
 toplam = 0
-
 for index in range(1000):
     if (index % 3 == 0 or index % 5 == 0):
         toplam += index
-
 print('Toplam:',toplam)
