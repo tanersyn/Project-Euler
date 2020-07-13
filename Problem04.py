db439af78db6dc593dd36975e9df12cddeb50a12
Taner Sayın <50462952+tanersyn@users.noreply.github.com>
2020-07-13 23:17:53 +0300

    Update and rename Problem4.py to Problem04.py

--- Problem04.py
+++ Problem04.py
@@ -1,11 +1,8 @@
 def Hesaplayacı():
-
     cevap = max (i*j 
     for i in range(100,1000) 
     for j in range(100,1000)
     if str(i*j) == str(i*j)[::-1]   )
-
     return str(cevap)
-
 if __name__ == "__main__":
 	print(Hesaplayacı())
